c93fee7f87c072642de52796dd226ad87f1fc02d
wonoh <wldnjsdh44121@nate.com>
2019-02-20 13:59:54 +0900

    11718 그대로출력하기 scanner 사용

---
+++ src/baekjoon/Baekjoon11718.java
@@ -0,0 +1,22 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Baekjoon11718 {
+    static Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        while(scan.hasNextLine()){
+            String input = scan.nextLine();
+            System.out.println(input);
+            if(input==null){
+                return;
+            }
+        }
+    }
+
+}
