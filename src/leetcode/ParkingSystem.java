d11dcc4a472e445d1881037532e6abe69085cd15
wonoh <wonoh@zuminternet.com>
2020-10-19 21:16:30 +0900

    리트코드 문제풀이

---
+++ src/leetcode/ParkingSystem.java
@@ -0,0 +1,43 @@
+package leetcode;
+
+public class ParkingSystem {
+
+    private int big;
+    private int medium;
+    private int small;
+
+    public ParkingSystem(int big, int medium, int small) {
+        this.big = big;
+        this.medium = medium;
+        this.small = small;
+    }
+
+    public boolean addCar(int carType) {
+        if(carType == 1){
+            if(big > 0){
+                big--;
+                return true;
+            }else{
+                return false;
+            }
+        }else if(carType == 2){
+            if(medium > 0){
+                medium--;
+                return true;
+            }else{
+                return false;
+            }
+        }else{
+            if(small > 0){
+                small--;
+                return true;
+            }else{
+                return false;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        ParkingSystem parkingSystem = new ParkingSystem(1,1,0);
+    }
+}
